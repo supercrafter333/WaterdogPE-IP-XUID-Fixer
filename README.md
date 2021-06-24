3c4e4be4978fd94371f04363f3e39aad52eef287
supercrafter333 <justcoolstuff99@gmail.com>
2021-06-24 22:42:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# WaterdogPE-IP-XUID-Fixer - V1.0.0
+<hr />
+
+A PocketMine-MP plugin that fixes the wrong setted Ip and Xuid player-settings in PocketMine-MP caused by Waterdog(PE).
+
+### What does this fix?
+
+##### IP
+Before (unfixed): server-ip
+
+Now (fixed): player-ip
+
+<br />
+
+##### XUID
+Before (unfixed): ""
+
+Now (fixed): player-xuid
+
+
+### License
+This plugin is licensed under the [Apache License 2.0](/LICENSE) by supercrafter333.
---
+++ src/supercrafter333/WaterdogPEFixer/WaterdogPEFixer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace supercrafter333\WaterdogPEFixer;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\RakLibInterface;
+use pocketmine\plugin\PluginBase;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionProperty;
+
+class WaterdogPEFixer extends PluginBase implements Listener
+{
+
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    #################################
+    ##[Fix Waterdog(PE) IP & XUID ]##
+    #################################
+    public function onPacketReceive(DataPacketReceiveEvent $event): void {
+        $packet = $event->getPacket();
+        if($packet instanceof LoginPacket) {
+            foreach ( $this->getServer()->getNetwork()->getInterfaces() as $interface ) {
+                if ( $interface instanceof RakLibInterface ) {
+                    try {
+                        $reflector = new ReflectionProperty( $interface, "interface" );
+                        $reflector->setAccessible( true );
+                        $reflector->getValue( $interface )->sendOption( "packetLimit", 900000000000 );
+                    } catch ( ReflectionException $e ) {}
+                }
+            }
+            if(isset($packet->clientData["Waterdog_IP"])) {
+                $class = new ReflectionClass($event->getPlayer());
+
+                $prop = $class->getProperty("ip");
+                $prop->setAccessible(true);
+                $prop->setValue($event->getPlayer(), $packet->clientData["Waterdog_IP"]);
+            }
+            if (isset($packet->clientData["Waterdog_XUID"])) {
+                $class = new ReflectionClass($event->getPlayer());
+
+                $prop = $class->getProperty("xuid");
+                $prop->setAccessible(true);
+                $prop->setValue($event->getPlayer(), $packet->clientData["Waterdog_XUID"]);
+                $packet->xuid = $packet->clientData["Waterdog_XUID"];
+            }
+        }
+    }
+    #################################
+    #################################
+    #################################
+}
